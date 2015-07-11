8395050ab005370056065b4041c5dd9a08c003c2
Swami Iyer <swami.iyer@gmail.com>
2015-07-11 18:30:07 -0400

    Merged code from prepare_data2.py.

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -6,6 +6,10 @@ import sys
 # following (56498) rows are bit vectors corresponding to the 
 # cuisine-ingredient rows from the input (srep00196-s3.csv) file.
 #
+# The script then creates files X.csv and Y.csv: the former contains  
+# one-hot vectors u corresponding to each bit vector v of food2vec.csv, and 
+# the latter stores the corresponding (context) bit vectors (v - u).
+#
 # Usage: python prepare_data.py
 
 def main(args):
@@ -27,6 +31,24 @@ def main(args):
         bits = ["1" if word in toks else "0" for word in sorted_words]
         fh.write("%s\n" %(",".join(bits)))
     fh.close()
+
+    fh = open("food2vec.csv", "r")
+    lines = fh.readlines()
+    fh.close()
+    Xfh = open("X.csv", "w")
+    Yfh = open("Y.csv", "w")
+    for line in lines[1:]:
+        toks = line.strip().split(",")
+        hot_indices = [i for i, x in enumerate(toks) if x == "1"]
+        for i in hot_indices:
+            x_vector = ["0"] * len(toks)
+            x_vector[i] = "1"
+            y_vector = [tok for tok in toks]
+            y_vector[i] = "0"
+            Xfh.write("%s\n" %(",".join(x_vector)))
+            Yfh.write("%s\n" %(",".join(y_vector)))
+    Xfh.close()
+    Yfh.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
