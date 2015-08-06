c09768e86102ae08d882dcc6e05954c4ad26aa0a
henryzlo <henryzlo@gmail.com>
2015-08-05 22:49:16 -0400

    removed scuba / theano.  single core cpu is sufficient. implemented skipgram without negative sampling.

--- scuba2/__init__.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'henryzlo'
-
-
-from data import load_mnist, load_cifar10
-from layers import *
-from mlp import *
-from scuba2.mlp import MLP
---
+++ skipgram.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+
+# We use the skip-gram model without negative sampling
+# reference: http://www-personal.umich.edu/~ronxin/pdf/w2vexp.pdf
+
+# Initialization of W is taken from the reference implementation:
+# https://groups.google.com/forum/#!topic/word2vec-toolkit/du6teCT5Pug
+
+# Code reference:
+# https://github.com/piskvorky/gensim/blob/develop/gensim/models/word2vec.py
+
+# linearly reduce this to 0
+LR = 0.025
+NUM_EPOCHS = 3
+K = 25
+
+def build_indices():
+    "Return a dict.  Dict assigns a number to each word."
+    indices= {}
+    i = 0
+    for line in open('data/srep00196-s3.csv','r'):
+        # skip header
+        if '#' not in line:
+            words = line.rstrip().split(',')            
+            for word in words:
+                if word not in indices:
+                    indices[word] = i
+                    i = i + 1
+    return indices
+
+def line_generator(indices):
+    "Generator, yield list of word indices for each line in data."
+    for line in open('data/srep00196-s3.csv','r'):
+        # skip headers
+        if '#' not in line:
+            tokens = line.rstrip().split(',')            
+            yield [indices[t] for t in tokens]
+
+
+if __name__ == '__main__':
+    indices = build_indices()
+    N = len(indices)
+    W = np.random.uniform(low=-0.5/K, high=0.5/K, size=(N, K))
+
+    # use each food (food_x) to predict others (food_y) in the same recipe
+    for epoch in xrange(NUM_EPOCHS):        
+        total_loss = 0.0 # per line
+        num_lines = 0
+        for recipe in line_generator(indices):
+            for food_x in recipe:
+                # calculate softmax(X*W*W^T) for food_x
+                output = 1. /(1. + np.exp(-np.dot(W[food_x], W.T)))
+                target = np.zeros(N)
+                target[recipe] = 1.
+                target[food_x] = 0.
+
+                # calculate error wrt predicting food_y
+                grad = output - target
+                # first W update (hidden->output)
+                W -= LR * np.outer(grad, W[food_x])
+                # input->hidden update
+                W[food_x] -= LR * np.dot(grad, W)
+
+                total_loss += np.mean(grad)
+    
+    # print result
+    i = 0
+    i2word = {v: k for k, v in indices.items()}
+    for row in W:
+        tokens = [i2word[i]]
+        tokens.extend(row.tolist())
+        print ','.join([str(t) for t in tokens])
+        i += 1
